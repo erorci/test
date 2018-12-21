4988a7ac1082a46af61b8b6d5bb481985a9bfe6b
Emerson Orci <emerson.orci@la.logicalis.com>
2018-12-21 09:41:24 -0200

    test

--- mount-volume.sh
+++ mount-volume.sh
@@ -1,3 +1,3 @@
 mkdir /mnt-coopercitrus
-echo '//coopercitrusstoragedev.file.core.windows.net/coopercitrusdev/ /mnt-coopercitrus/      cifs    vers=3.0,username=coopercitrusstoragedev,password=$1,dir_mode=0777,file_mode=0777,sec=ntlmssp       0       0' >> /etc/fstab
-mount -a
+echo '//coopercitrusstoragedev.file.core.windows.net/coopercitrusdev/ /mnt-coopercitrus/      cifs    vers=3.0,username=coopercitrusstoragedev,password=$1,dir_mode=0777,file_mode=0777,sec=ntlmssp       0       0' | sudo tee -a  /etc/fstab
+sudo mount -a
